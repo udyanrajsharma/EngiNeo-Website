332d49ae1109bb53be6fd1b9420184ee69906ab7
Neelmani <neelmanip@engineosol.com>
2024-03-04 10:24:08 +0530

    logo setup

--- home.js
+++ home.js
@@ -74,3 +74,79 @@ document.addEventListener('DOMContentLoaded', function() {
   // Check initially
   checkInView();
 });
+
+
+
+// nsection2
+
+const part1 = document.querySelector('.part1');
+// Select the nsec2text1 paragraph
+const nsec2text1 = document.querySelector('.nsec2textr1');
+
+// Add event listener for mouseover on part1
+part1.addEventListener('mouseover', function() {
+    // Show the nsec2text1 paragraph
+    nsec2text1.style.display = 'block';
+});
+
+// Add event listener for mouseout on part1
+part1.addEventListener('mouseout', function() {
+    // Hide the nsec2text1 paragraph
+    nsec2text1.style.display = 'none';
+});
+
+
+
+
+const part2 = document.querySelector('.part2');
+// Select the nsec2text1 paragraph
+const nsec2text2 = document.querySelector('.nsec2textr2');
+
+// Add event listener for mouseover on part1
+part2.addEventListener('mouseover', function() {
+    // Show the nsec2text1 paragraph
+    nsec2text2.style.display = 'block';
+});
+
+// Add event listener for mouseout on part1
+part2.addEventListener('mouseout', function() {
+    // Hide the nsec2text1 paragraph
+    nsec2text2.style.display = 'none';
+});
+
+
+
+const part3 = document.querySelector('.part3');
+// Select the nsec2text1 paragraph
+const nsec2text3 = document.querySelector('.nsec2textr3');
+
+// Add event listener for mouseover on part1
+part3.addEventListener('mouseover', function() {
+    // Show the nsec2text1 paragraph
+    nsec2text3.style.display = 'block';
+});
+
+// Add event listener for mouseout on part1
+part3.addEventListener('mouseout', function() {
+    // Hide the nsec2text1 paragraph
+    nsec2text3.style.display = 'none';
+});
+
+
+
+
+const part4 = document.querySelector('.part4');
+// Select the nsec2text1 paragraph
+const nsec2text4 = document.querySelector('.nsec2textr4');
+
+// Add event listener for mouseover on part1
+part4.addEventListener('mouseover', function() {
+    // Show the nsec2text1 paragraph
+    nsec2text4.style.display = 'block';
+});
+
+// Add event listener for mouseout on part1
+part4.addEventListener('mouseout', function() {
+    // Hide the nsec2text1 paragraph
+    nsec2text4.style.display = 'none';
+});
